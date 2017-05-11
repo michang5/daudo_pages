01be19384daec7a23e76e2834038a65ef0bc152d
michang5 <immi5.tw@gmail.com>
2017-05-11 17:27:48 +0800

    購買頁

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-	$('[href="#top"], [href="#sport-1"], [href="#sport-2"], [href="#sport-3"]').click(function () {
+	$('[href="#section-7"]').click(function () {
 		if ($(this).attr('href') == '#') {} else {
 			$('html, body').animate({
 				scrollTop: $($(this).attr('href')).offset().top - 50
